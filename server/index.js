2b619a004b82aece0eb3e20a8efaa949031ceacb
ATEKSA <atulikamikabeidze9@gmail.com>
2026-06-12 04:13:50 +0400

    Fix Express 5 routing and restore Home/Player text

--- server/index.js
+++ server/index.js
@@ -419,7 +419,7 @@ io.on('connection', (socket) => {
 
 // Serve Vite Frontend in Production
 app.use(express.static(path.join(__dirname, '../dist')));
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
